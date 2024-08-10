0bbd80754b07f9c62aca81ca89ec4c4b523b9384
halitgilbaris <142479984+halitgilbaris@users.noreply.github.com>
2024-08-11 01:05:08 +0300

    Update şifre kırma2.py

--- şifre kırma2.py
+++ şifre kırma2.py
@@ -1,25 +1,32 @@
-
-şifre = "11111"
 flag = False
+şifre = "dj92"
 
-rakamlar = "0123456789"
+sayılar = "abcçdefgğhiıjklmnoöprsştuüvyz0123456789"
 
-for basamak0 in rakamlar:
-    for basamak1 in rakamlar:
-        for basamak2 in rakamlar:
-            for basamak3 in rakamlar:
-                for basamak4 in rakamlar:
-                    deneme = (basamak0+basamak1+basamak2+basamak3+basamak4)
-                    print(deneme)
-                    if deneme == şifre:
-                        print("Parolanız: " + deneme)
-                        flag = True
-                        break
-                if flag == True:
-                    break
+for basamak0 in sayılar:
+    for basamak1 in sayılar:
+        for basamak2 in sayılar:
+            for basamak3 in sayılar:
+                for basamak4 in sayılar:
+                    for basamak5 in sayılar:
+                        for basamak6 in sayılar:
+                            deneme = (basamak0+basamak1+basamak2+basamak3+basamak4+basamak5+basamak6)
+                            print(deneme)
+                            if deneme == şifre:
+                                print("Parolanız: " + deneme)
+                                flag = True
+                        if flag == True:
+                            break
+                    if flag == True:
+                       break
+               if flag == True:
+                   break
             if flag == True:
-                    break
+                break
         if flag == True:
-                    break
+            break
     if flag == True:
-                    break
+        break
+                    
+
+
