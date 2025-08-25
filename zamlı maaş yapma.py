50f9d1a6f1a009efa3feb05b789b73b464fade1d
halitgilbaris <142479984+halitgilbaris@users.noreply.github.com>
2025-08-25 12:57:49 +0300

    Delete zamlı maaş yapma.py

--- zamlı maaş yapma.py
+++
@@ -1,8 +0,0 @@
-# Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteren Python örneği
-
-while True:
-    MAAS = int(input("maaşı girin"))
-    ZAM_ORANI = int(input("zam oranı(%)"))
-    YENI_MAAS = int(MAAS) + int(MAAS)*int(ZAM_ORANI)/100
-
-    print("Zamlı maaş: ", YENI_MAAS)
