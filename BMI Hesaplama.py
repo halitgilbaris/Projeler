2f80f5c89104304b49aa441613f64c1b479aafa1
halitgilbaris <142479984+halitgilbaris@users.noreply.github.com>
2025-08-25 12:53:00 +0300

    Update BMI Hesaplama.py

--- BMI Hesaplama.py
+++ BMI Hesaplama.py
@@ -1,14 +1,1 @@
-def BMI():
-    Boy = float(input("Lütfen Boyunuzu Giriniz: "))
-    Kilo = int(input("Lütfen Kilonuzu Giriniz: "))
-    bmi = float((Kilo / (Boy * Boy)))
-    print(bmi)
-    pass
 
-
-while True:
-    panel = int(input("1 - BMI Hesaplama\n2 - Çıkış\n>>"))
-    if panel == 1:
-        BMI()
-    elif panel == 2:
-        break
